e8c332156b986f94bed121aeaf059d55e92a7f4f
Tim Slippy, voxelv <slippytim@gmail.com>
2017-07-15 20:39:23 -0700

    lwjgl3 test

--- desktop/src/com/derelictech/bitcorfab/desktop/DesktopLauncher.java
+++ desktop/src/com/derelictech/bitcorfab/desktop/DesktopLauncher.java
@@ -2,14 +2,15 @@ package com.derelictech.bitcorfab.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.derelictech.bitcorfab.BitCorFab;
 import com.derelictech.bitcorfab.CONST;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = (int)CONST.SCREEN_W;
-		config.height = (int)CONST.SCREEN_H;
-		new LwjglApplication(new BitCorFab(), config);
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setWindowedMode((int)CONST.SCREEN_W, (int)CONST.SCREEN_H);
+		new Lwjgl3Application(new BitCorFab(), config);
 	}
 }
