8cba05a6975b4b83da5542aa3a4ad03bd06e4b75
Tim Slippy, voxelv <slippytim@gmail.com>
2017-07-17 22:52:53 -0700

    litelitelite recommendation

--- core/src/com/derelictech/bitcorfab/Screen1.java
+++ core/src/com/derelictech/bitcorfab/Screen1.java
@@ -8,12 +8,15 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.HdpiUtils;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 /**
  * Project: bitcorfab
@@ -24,17 +27,27 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
  */
 public class Screen1 extends ScreenAdapter {
     Image img;
+    Image img2;
     Stage stage;
 
     public Screen1() {
-        stage = new Stage( new FitViewport(CONST.WORLD_W, CONST.WORLD_H));
+        stage = new Stage( new StretchViewport(CONST.WORLD_W, CONST.WORLD_H));
 
         Gdx.input.setInputProcessor(stage);
 
+        // Image no stretch (maintain aspect ration of original image)
         img = new Image(new Texture("test_img.png"));
-        img.setPosition(0.0f, 0.0f);
-        img.setSize(32.0f, 32.0f);
+        Vector2 imageSize = Scaling.fit.apply(CONST.WORLD_W, CONST.WORLD_W, CONST.WORLD_W, CONST.WORLD_H);
+        img.setSize(imageSize.x, imageSize.y);
+        img.setPosition((CONST.WORLD_W - imageSize.x) / 2, (CONST.WORLD_H - imageSize.y) / 2f);
         stage.addActor(img);
+
+        // Image stretch
+        img2 = new Image(new Texture("test_img.png"));
+        img2.setSize(CONST.WORLD_W, CONST.WORLD_H);
+        img2.setPosition(0f, 0f);
+        stage.addActor(img2);
+        stage.setDebugAll(true);
 
         debugGraphicsCalls();
     }
