f2d9dbe55afa9d4e8656c2803f71c5dfe9651958
Tim Slippy, voxelv <slippytim@gmail.com>
2017-07-21 23:31:17 -0700

    made a clicky button

--- core/src/com/derelictech/bitcorfab/Screen1.java
+++ core/src/com/derelictech/bitcorfab/Screen1.java
@@ -1,22 +1,14 @@
 package com.derelictech.bitcorfab;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.HdpiUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 /**
  * Project: bitcorfab
@@ -27,29 +19,31 @@ import com.badlogic.gdx.utils.viewport.StretchViewport;
  */
 public class Screen1 extends ScreenAdapter {
     Image img;
-    Image img2;
     Stage stage;
+    VoxButton button1;
 
     public Screen1() {
-        stage = new Stage( new StretchViewport(CONST.WORLD_W, CONST.WORLD_H));
+        stage = new Stage( new FitViewport(CONST.WORLD_W, CONST.WORLD_H));
 
         Gdx.input.setInputProcessor(stage);
 
-        // Image no stretch (maintain aspect ration of original image)
-        img = new Image(new Texture("test_img.png"));
-        Vector2 imageSize = Scaling.fit.apply(CONST.WORLD_W, CONST.WORLD_W, CONST.WORLD_W, CONST.WORLD_H);
-        img.setSize(imageSize.x, imageSize.y);
-        img.setPosition((CONST.WORLD_W - imageSize.x) / 2, (CONST.WORLD_H - imageSize.y) / 2f);
-        stage.addActor(img);
+//        // Image no stretch (maintain aspect ration of original image)
+//        img = new Image(new Texture("test_img.png"));
+//        Vector2 imageSize = Scaling.fit.apply(CONST.WORLD_W, CONST.WORLD_W, CONST.WORLD_W, CONST.WORLD_H);
+//        img.setSize(imageSize.x, imageSize.y);
+//        img.setPosition(
+//                (CONST.WORLD_W - imageSize.x) / 2.0f,
+//                (CONST.WORLD_H - imageSize.y) / 2.0f
+//        );
+//        stage.addActor(img);
 
-        // Image stretch
-        img2 = new Image(new Texture("test_img.png"));
-        img2.setSize(CONST.WORLD_W, CONST.WORLD_H);
-        img2.setPosition(0f, 0f);
-        stage.addActor(img2);
-        stage.setDebugAll(true);
+        button1 = new VoxButton("buttons/button_style1.png", "buttons/button_style1_down.png");
+        button1.setPosition(32, 32);
+        button1.setSize(8, 6);
+        stage.addActor(button1);
 
-        debugGraphicsCalls();
+
+        // debugGraphicsCalls();
     }
 
     @Override
@@ -61,7 +55,7 @@ public class Screen1 extends ScreenAdapter {
     @Override
     public void resize(int width, int height) {
         stage.getViewport().update(width, height);
-        debugGraphicsCalls();
+        //debugGraphicsCalls();
     }
 
     private void debugGraphicsCalls() {
---
+++ core/src/com/derelictech/bitcorfab/VoxButton.java
@@ -0,0 +1,59 @@
+package com.derelictech.bitcorfab;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+/**
+ * Project: bitcorfab
+ * Package: com.derelictech.bitcorfab
+ * Author:  voxelv
+ * Creation Date: 2017-07-21
+ * Description:
+ */
+public class VoxButton extends Actor {
+    TextureRegion buttonUp;
+    TextureRegion buttonDown;
+    boolean pressed = false;
+
+    public VoxButton(String buttonUpFilename, String buttonDownFilename) {
+        buttonUp = new TextureRegion(new Texture(Gdx.files.internal(buttonUpFilename)));
+        buttonDown = new TextureRegion(new Texture(Gdx.files.internal(buttonDownFilename)));
+
+        setSize(6, 4);
+
+        addListener(new ClickListener(Input.Buttons.LEFT){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                boolean returnValue = super.touchDown(event, x, y, pointer, button);
+                pressed = true;
+                return returnValue;
+            }
+
+            @Override
+            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+                pressed = false;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+
+                pressed = false;
+            }
+        });
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.setColor(Color.RED);
+        batch.draw(pressed ? buttonDown : buttonUp, getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(),
+                   getScaleX(), getScaleY(), getRotation());
+        Gdx.app.debug("BTN", "DRAW VoxButton");
+    }
+}
